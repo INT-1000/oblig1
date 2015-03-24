46aa7fc9342638c1d3a94ac1254b15270ea79c97
bjorntorjus <bjornt.ilestad@gmail.com>
2015-03-24 13:04:16 +0100

    tror jeg fiksa filene som manglet
    vet ikke hva jeg gjør

---
+++ session/side2.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+?>
+<form method="post" action="side3.php"> 
+	Hva er 3+1? <input type="number" id="svar" name="svar" required /> <br/>
+	<input type="submit" value="Fortsett" name="regsvar" >
+</form>
+
+<?php
+@$regsvar=$_POST["regsvar"];
+
+if($regsvar)
+{
+	$svar=$_POST["svar"];							//Henter input fra html skjema
+	$poeng=$_SESSION["poeng"];						//setter Session-variabel
+	if($svar=="2")									//GIR DEG SVAR PÅ FORRIGE side1.php
+	{
+		print("$svar er korrekt<br/>");				
+		$poeng++;									//øker poeng med 1
+		$_SESSION["poeng"]=$poeng;					//Lagrer Session variabel etter endring
+		print("du har $poeng poeng!<br/>");			//sier hvor mange poeng du har
+	}
+
+	else 											
+	{
+		print("$svar er feil. 2 er riktig<br/>");	//Sier hva du svarte og hva som var korrekt
+		print("du har $poeng poeng!<br/>");		
+	}
+print ("<a href='side1.php'>Start på nytt</a><br/>");
+print ("<a href='avslutt.php'>Nullstill alt</a><br/>");
+$poeng=$_SESSION["poeng"];
+}
+?>
