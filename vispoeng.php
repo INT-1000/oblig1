cd1cff15812932df591ee1710f5bae1bfc26fe27
serushan@live.com <Creativ8>
2015-04-06 23:31:32 +0200

    ITS THE FINAL COUNTDOWN
    SISTE OPPDATERINGEN

--- vispoeng.php
+++ vispoeng.php
@@ -16,9 +16,11 @@ $sql="SELECT brukernavn, poeng FROM bruker ORDER by poeng;";
 $sqlresultat=mysqli_query($dbcon, $sql) or die ("Ikke mulig å hente data fra databasen"); 
 $antallrader=mysqli_num_rows($sqlresultat); 
 
+print("<div class='innhold'>");
 print("<h3>POENG</h3>"); 
 print("<table border=1>");
 print("<tr><th align=left>Brukernavn</th> <th align=left>Poeng</th>");
+
 
 for($r=1; $r<=$antallrader; $r++)
 {
@@ -30,6 +32,7 @@ for($r=1; $r<=$antallrader; $r++)
 }
 
 print("</table>");
+print("</div>");
 
 
 }
